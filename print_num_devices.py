1e2d9374cb5be6d7127d29da29f412c68ebc95a7
Rasmus Larsen <rasmus.larsen@alexandra.dk>
2023-05-10 14:12:25 +0200

    update print num devices

--- print_num_devices.py
+++ print_num_devices.py
@@ -1,5 +1,30 @@
 import jax
+import socket
+from jax.experimental import multihost_utils
+import portpicker
 
-jax.distributed.initialize("34.69.59.135:1234", num_processes=2, process_id=jax.process_index())
+def gen_local_ip():
+    hostname = socket.gethostname()
+    return socket.gethostbyname(hostname)
 
-print(jax.device_count())
+def gen_local_ip_nums():
+    return [int(num) for num in gen_local_ip().split(':')[-1].split('.')]
+
+def get_coordinator_ip():
+    local_ip_nums = jax.numpy.array(gen_local_ip_nums())
+    coordinator_ip_nums = multihost_utils.broadcast_one_to_all(local_ip_nums)
+    coordinator_ip_strings = [str(num) for num in list(coordinator_ip_nums)]
+    return '.'.join(coordinator_ip_strings)
+
+port = multihost_utils.broadcast_one_to_all(jax.numpy.array(portpicker.pick_unused_port()))
+coordinator_address = get_coordinator_ip() + ':' + str(port)
+jax.distributed.initialize(coordinator_address=coordinator_address,
+                            num_processes=jax.process_count(),
+                            process_id=jax.process_index())
+
+print("jax.process_index():", jax.process_index())
+print("jax.process_count():", jax.process_count())
+print("jax.host_id():", jax.host_id())
+print("jax.host_count():", jax.host_count())
+print("jax.local_device_count():", jax.local_device_count())
+print("jax.device_count():", jax.device_count())
